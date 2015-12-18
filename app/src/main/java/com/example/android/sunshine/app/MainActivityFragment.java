9908f0b386a5e329d8460ad3a566f1f0621ff5a2
Atinder Singh <majhalhouse@gmail.com>
2015-12-18 09:28:59 +0530

    Changed the layout and added arraylist

--- app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -6,17 +6,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ArrayList<String> weekForecast= new ArrayList<>();
+        weekForecast.add("Today-Sunny-88/63");
+        weekForecast.add("Tomorrow-Foggy-70/46");
+        weekForecast.add("Weds-Cloudy-72/63");
+        weekForecast.add("Thurs-Rainy-64/51");
+        weekForecast.add("Fri-Foggy-70/46");
+        weekForecast.add("Sat-Sunny-76/68");
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
