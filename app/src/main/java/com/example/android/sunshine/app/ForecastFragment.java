7a45f6e37e99847708c4535b3f4743f5e55aae99
Atinder Singh <majhalhouse@gmail.com>
2016-02-26 01:58:47 +0530

    Fixed the error

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -50,8 +50,12 @@ public class ForecastFragment extends Fragment {
 
     }
 
-    public boolean onOptionItemSelected(MenuItem item) {
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
             FetchWeatherTask weatherTask=new FetchWeatherTask();
