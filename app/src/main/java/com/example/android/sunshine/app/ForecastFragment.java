e1305c3dad4e099c781fde8f51585813bfaa1ed6
Atinder Singh <majhalhouse@gmail.com>
2016-02-27 01:48:33 +0530

    Added refresh button

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -149,8 +149,12 @@ public class ForecastFragment extends Fragment {
                 if (buffer.length() == 0) {
                     // Stream was empty.  No point in parsing.
                     forecastJsonStr = null;
+
                 }
                 forecastJsonStr = buffer.toString();
+
+                Log.v(LOG_TAG, "Forecasr JSON String:" + forecastJsonStr);
+
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attempting
